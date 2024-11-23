f086e687fe1c58da616be03bb3a4d4677ea4a32a
adilelmoba <adilelmoba20@gmail.com>
2024-11-23 10:52:03 +0100

    add/ bootstrap, fontawesome libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'netflix-clone';
+
+  faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faCircleInfo
+];
